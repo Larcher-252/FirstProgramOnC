9e5de009ce4abdd5c14ea490144c011e54fadd64
Void7 <limen191100@gmail.com>
2021-10-22 20:41:27 +0300

    Use comments
    Add one-line comments //
    Add multiline comments /* */

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,16 @@
+/*
+* hello.cpp
+* This program prints in terminal some information.
+* By Void7 <limen191100@gmail.com>
+*/
 #include <cstdio>
 
 int main()
 {
+    // Prints in terminal my name
     printf("Hello, my name is Aleksey.");
+    // Prints in terminal where I'm from
     printf("\nI'm from Saint-Petersburg, Russia.");
+    // Prints in terminal what I like
     printf("\nI like game developing.");
 }
